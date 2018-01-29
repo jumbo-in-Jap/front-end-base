e6f28f12d4e2fe450b05e17e4f8751f961b9195d
HanedaKentarou <khaneda.000@gmail.com>
2018-01-30 07:56:02 +0900

    add pug setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,58 +2,105 @@ var path                       = require('path');
 var ExtractTextPlugin          = require('extract-text-webpack-plugin');
 var BrowserSyncPlugin          = require('browser-sync-webpack-plugin');  
 var WebpackBuildNotifierPlugin = require('webpack-build-notifier'); 
+var Autoprefixer               = require('autoprefixer'); 
+
+var Webpack               = require('webpack'); 
+let extractStyles = new ExtractTextPlugin('[name].css');
+let extractHtml = new ExtractTextPlugin('[name].html');
 
 module.exports = {
+stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: true,
+    timings: true,
+    chunks: false,
+    chunkModules: false
+},
 entry: {
-    common: './src/sass/common.scss',
-    login: './src/sass/login.scss',
-    home: './src/sass/home.scss'
+    index: [
+      path.resolve(__dirname, './src/pug/index.pug')
+    ],
+    // index: [
+    //   path.resolve(__dirname, 'templates/index.pug')
+    // ],
+    // post: [
+    //   path.resolve(__dirname, 'templates/post.pug')
+    // ],    
+    'css/application': [
+      path.resolve(__dirname, './src/sass/common.scss')
+    ]
 },
 output: {
-    path: path.join(__dirname, 'dist/css'),
-    filename: '[name].css'
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
 },
 module: {
     rules: [
         {
-            test: /\.scss$/,
+            test: /\.pug$/,
             use: ExtractTextPlugin.extract({
+                use: [
+                    { loader: 'html-loader'},
+                    { loader: 'pug-html-loader?pretty&exports=false'}
+                ]
+            })
+        },
+        {
+            test: /\.scss$/,
+            use: extractStyles.extract({
                 use: [{
                     loader: "css-loader"
                 }, {
                     loader: "sass-loader"
                 }],
-                // use style-loader in development 
                 fallback: "style-loader"
             })
         },
         {
             test: require.resolve('jquery'),
             use: [
-              { loader: 'expose-loader', options: 'jQuery' },
-              { loader: 'expose-loader', options: '$' }
+            { loader: 'expose-loader', options: 'jQuery' },
+            { loader: 'expose-loader', options: '$' }
             ]
-          },          
-          {
+        },          
+        {
             test: require.resolve('tether'),
             use: [
-              { loader: 'expose-loader', options: 'Tether' }
+            { loader: 'expose-loader', options: 'Tether' }
             ]
-          }
+        }
     ]
 },
 plugins: [
-    new WebpackBuildNotifierPlugin(),
-    new ExtractTextPlugin('[name].css'),
     new BrowserSyncPlugin({
         host: 'localhost',
         port: 3000,
-        server: { baseDir: ['dist'] },
+        server: { baseDir: ['build'] },
         files: [
-            'dist/*.css',
-            'dist/html/*.html'
+            'build/css/*.css',
+            'build/*.html'
         ]
-    })
+    }),
+    new Webpack.LoaderOptionsPlugin({
+        minimize: false,
+        debug: true,
+        options: {
+            postcss: [
+            Autoprefixer({
+                browsers: ['last 2 version', 'Explorer >= 10', 'Android >= 4']
+            })
+            ],
+            sassLoader: {
+            includePaths: [
+                path.resolve(__dirname, 'node_modules/sanitize.css/')
+            ]
+            }
+        }
+        }),
+    extractStyles,
+    extractHtml
 ],
 externals: [
     {
